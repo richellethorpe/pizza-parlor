0449cd71231f0b177d11dc8790ea9c8ccb4c3d5c
Richelle Thorpe <richelle.ak@gmail.com>
2022-11-04 12:14:55 -0700

    it will update pizza constructor and add biz logic to determine size price

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,40 @@
 //Business Logic
-
-function PizzaToppings (meatTopping, sauceTopping, cheeseTopping, otherToppings, sizeOfPizza) {
-  this.meatTopping = meatTopping;
-  this.sauceTopping = sauceTopping;
-  this.cheeseTopping = cheeseTopping;
-  this.otherToppings = otherToppings;
-  this.sizeOfPizza = sizeOfPizza;
-}
-
 //user can select meat for $5 more, sauces and cheeses are free, other toppings are $1 more and size of pizza increases by $5 each size it goes up from small
 
-PizzaToppings.prototype.addMeatPricing = function() {
- if this.meatTopping === true;{
-  return price + 5
- }
+function Pizza (sizeSelction, toppingList) {
+  this.sizeSelction = sizeSelction
+  this.basePrice = 0
+  this.toppingList = toppingList
+  this.toppingListCost = 0
+  this.totalCost = 0
 }
+
+Pizza.prototype.sumBasePrice = function () {
+  if this.size === "small" {
+    this.basePrice = 12; 
+    } else if (this.size ==="medium") {
+      this.basePrice = 16;
+    } else if (this.size === "large") {
+      this.basePrice = 20;
+    }
+  };
+
+
+
 
 
 //UI Logic
 
 function handleForm(event) {
   event.preventDefault();
+
+  const sizeSelection =
+  const meatSelection =
+  const cheseSelection = 
+  const toppingSelection =
+  const price = 
+
+
 
   
 }
