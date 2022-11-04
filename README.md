b89bd84da9e0d2791a7f79c4188d465c41b5b1bf
Richelle Thorpe <richelle.ak@gmail.com>
2022-11-04 08:45:03 -0700

    add constructor for pizza options in business logic

---
+++ README.md
@@ -0,0 +1,1 @@
+//create option for user to 
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Business Logic
+
+function PizzaToppings (meatTopping, sauceTopping, cheeseTopping, otherToppings, sizeOfPizza) {
+  this.meatTopping = meatTopping;
+  this.sauceTopping = sauceTopping;
+  this.cheeseTopping = cheeseTopping;
+  this.otherToppings = otherToppings;
+  this.sizeOfPizza = sizeOfPizza;
+}
+
+//user can select meat for $5 more, sauces and cheeses are free, other toppings are $1 more and size of pizza increases by $5 each size it goes up from small
