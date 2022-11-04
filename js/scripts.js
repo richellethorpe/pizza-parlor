30941455bbf0b5231e013f1e1911c2f713c7213a
Richelle Thorpe <richelle.ak@gmail.com>
2022-11-04 14:25:59 -0700

    WIP business logic to calc size price

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,32 @@
 // //Business Logic
 // //user can select meat for $5 more, sauces and cheeses are free, other toppings are $1 more and size of pizza increases by $5 each size it goes up from small
 
-// function Pizza (sizeSelction, toppingList) {
-//   this.sizeSelction = sizeSelction
-//   this.basePrice = 0
-//   this.toppingList = toppingList
-//   this.toppingListCost = 0
-//   this.totalCost = 0
-// }
+function Pizza(sizeSelection, meatSelection, cheeseSelction, toppingSelection) {
+  this.sizeSelection = sizeSelection
+  this.meatSelection = meatSelection
+  this.cheeseSelection = cheeseSelction
+  this.toppingSeletion = toppingSelection
+  this.basePrice= 0
+  
+}
 
-// Pizza.prototype.sumBasePrice = function () {
-//   if this.size === "small" {
-//     this.basePrice = 12; 
-//     } else if (this.size ==="medium") {
-//       this.basePrice = 16;
-//     } else if (this.size === "large") {
-//       this.basePrice = 20;
-//     }
-//   };
+Pizza.prototype.findBasePrice = function () {
 
-//   Pizza.prototype
+  if (this.sizeSelection === "small") {
+    this.basePrice =12;
+    } else if (this.sizeSelction ==="medium") {
+    this.basePrice =16;
+    } else if (this.sizeSelction === "large") {
+    this.basePrice = 20;
+    }
+    console.log(this.basePrice);
+  };
 
+let newZa = new Pizza("large", "pepperoni", "cheddar", "olives");
 
+function getPrice(pizza){
+
+}
 
 
 //UI Logic
@@ -29,16 +34,16 @@
 function handleSubmit(event) {
   event.preventDefault();
 
-  const sizeSelection = document.getElementById("size-options").value;
-  const meatSelection = document.querySelector("input[name=meat-options]:checked").value;
-  const cheseSelection = document.querySelector("input[name=cheese-options]:checked").value;
-  const toppingSelection =document.querySelector("input[name=topping-options]:checked").value;
+  const sizeSelectionValue = document.getElementById("size-options").value;
+  const meatSelectionValue = document.querySelector("input[name=meat-options]:checked").value;
+  const cheseSelectionValue = document.querySelector("input[name=cheese-options]:checked").value;
+  const toppingSelectionValue =document.querySelector("input[name=topping-options]:checked").value;
   //const price = "";
 
-  document.getElementById("sizeSelectionDisplay").innerText = sizeSelection;
-  document.getElementById("meatSelectionDisplay").innerText = meatSelection;
-  document.getElementById("cheeseSelectionDisplay").innerText = cheseSelection;
-  document.getElementById("toppingSelectionDisplay").innerText = toppingSelection;
+  document.getElementById("sizeSelectionDisplay").innerText = sizeSelectionValue;
+  document.getElementById("meatSelectionDisplay").innerText = meatSelectionValue;
+  document.getElementById("cheeseSelectionDisplay").innerText = cheseSelectionValue;
+  document.getElementById("toppingSelectionDisplay").innerText = toppingSelectionValue;
 
 
   document.getElementById('orderSummary').removeAttribute('class');
