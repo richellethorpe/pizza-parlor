6629e4eb0966e011f2c80fd2f4e2bd3d44881cc4
Richelle Thorpe <richelle.ak@gmail.com>
2022-11-06 12:21:55 -0800

    update buttons to checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,11 @@
-// // //Business Logic
-// // //user can select meat for $5 more, sauces and cheeses are free, other toppings are $1 more and size of pizza increases by $5 each size it goes up from small
+// Business Logic
 
-function Pizza(size, toppings) {
-  this.size = size;
+function Pizza (toppings, size) {
   this.toppings = toppings;
-  this.toppingsPrice = {}
+  this.size = size;
+  }
   
-};
-
-// function PizzaAddOns(meat, cheese, )
-Pizza.prototype.sizePrice = function() {
+  Pizza.prototype.getCost = function() {
   let sizePrice = 12
   if (this.size ==="small"){
     return sizePrice;
@@ -18,35 +14,45 @@ Pizza.prototype.sizePrice = function() {
   }else if(this.size === "large") {
     return sizePrice +8;
   }
-
-};
-
-// const pizza1 = new Pizza("medium", "peperoni", "mozzarella", "olives");
-
-//UI Logic
-
-function handleSubmit(event) {
-  event.preventDefault();
-
-  const sizeSelectionValue = document.getElementById("size-options").value;
-  const meatSelectionValue = document.querySelector("input[name=meat-options]:checked").value;
-  const cheseSelectionValue = document.querySelector("input[name=cheese-options]:checked").value;
-  const toppingSelectionValue =document.querySelector("input[name=topping-options]:checked").value;
-  //const price = "";
-  const toppingSelectionPrice =document.querySelector("input[name=topping-options]:checked").price;
-
-  document.getElementById("sizeSelectionDisplay").innerText = sizeSelectionValue;
-  document.getElementById("meatSelectionDisplay").innerText = meatSelectionValue;
-  document.getElementById("cheeseSelectionDisplay").innerText = cheseSelectionValue;
-  document.getElementById("toppingSelectionDisplay").innerText = toppingSelectionValue;
-  console.log(toppingSelectionPrice);
-
-
-  document.getElementById('orderSummary').removeAttribute('class');
+  
+    let toppingsCost = 0;
+    if (pizza.toppings.includes('spinach') || pizza.toppings.includes('olives') || pizza.toppings.includes('mushrooms') || pizza.toppings.includes('tomatos')) {
+      toppingsCost += 0.50; }
+    if (pizza.toppings.includes('pepperoni') || pizza.toppings.includes('sausage') || pizza.toppings.includes('ham')) {
+      toppingsCost += 1.00; }
+    if (pizza.toppings.includes('truffle-oil')) {
+      toppingsCost += 2.00; }
+  
+    let totalCost = sizePrice + toppingsCost;
+    return totalCost;
+  };
+  
+  // UI Logic
+  
+  function handlePizzaOrder(e) {
+    e.preventDefault();
+  
+    const paragraph = document.createElement("p");
+    const h2 = document.createElement("h2");
+    const pizzaSize = document.getElementById("pizza-size").value;
+    let toppingInput = document.querySelectorAll("input[name=toppings]:checked"); // NodeList
+    let toppingArray = Array.from(toppingInput);
+    const toppings = [];
+  
+    toppingArray.forEach(function(element) {
+      toppings.push(element.value);
+    });
+  
+    let pizzaOrder = new Pizza(toppings, pizzaSize);
+    console.log(pizzaOrder)
+    let costOf = pizzaOrder.getCost(pizzaOrder)
+    console.log(costOf);
+    h2.append("Your pizza has been submitted.");
+    paragraph.append("Your total is $" + costOf +". Made with love and turtle power");
+    document.body.append(h2, paragraph);
   }
-
-
-window.addEventListener('load', function() {
-  const form = document.querySelector("form");
-  form.addEventListener('submit', handleSubmit);
-})
+  
+  
+  window.addEventListener("load", function () {
+    document.querySelector("form#orderUp").addEventListener("submit", handlePizzaOrder);
+  });
