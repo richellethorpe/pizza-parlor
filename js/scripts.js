7bff9c43da10d2ba3d4fab1fbbdfdfb032c8562c
Richelle Thorpe <richelle.ak@gmail.com>
2022-11-06 17:48:55 -0800

    WIP Business logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function Pizza (toppings, size) {
   this.size = size;
   }
   
-  Pizza.prototype.price = function(pizza) {
+  Pizza.prototype.getCost = function() {
   let sizePrice = 12
   if (this.size ==="small"){
     return sizePrice;
@@ -15,55 +15,44 @@ function Pizza (toppings, size) {
     return sizePrice +8;
   }
   
-    let toppingsPrice = 0;
+    let toppingsCost = 0;
     if (pizza.toppings.includes('spinach') || pizza.toppings.includes('olives') || pizza.toppings.includes('mushrooms') || pizza.toppings.includes('tomatos')) {
-      toppingsPrice += 0.50; }
+      toppingsCost += 0.50; }
     if (pizza.toppings.includes('pepperoni') || pizza.toppings.includes('sausage') || pizza.toppings.includes('ham')) {
-      toppingsPrice += 1.00; }
+      toppingsCost += 1.00; }
     if (pizza.toppings.includes('truffle-oil')) {
-      toppingsPrice += 2.00; }
-    let totalCost = sizePrice + toppingsPrice;
-    return totalCost;
-    
+      toppingsCost += 2.00; }
+  
+    let totalCost = sizePrice + toppingsCost;
+    console.log(totalCost);
   };
   
   // UI Logic
-
-  function handlePizzaOrder(event) {
-    event.preventDefault();
-    
+  
+  function handlePizzaOrder(e) {
+    e.preventDefault();
   
     const paragraph = document.createElement("p");
     const h2 = document.createElement("h2");
     const pizzaSize = document.getElementById("pizza-size").value;
-    let toppingInput = document.querySelectorAll("input[name=toppings]:checked");
-    console.log(toppingInput);
+    let toppingInput = document.querySelectorAll("input[name=toppings]:checked"); // NodeList
     let toppingArray = Array.from(toppingInput);
-    let toppings = [];
-    
+    const toppings = [];
+  
     toppingArray.forEach(function(element) {
       toppings.push(element.value);
     });
-    console.log("these are the", toppings);
+  
     let pizzaOrder = new Pizza(toppings, pizzaSize);
     console.log(pizzaOrder)
-    let costOf = pizzaOrder.price(pizzaOrder)
+    let costOf = pizzaOrder.getCost(pizzaOrder)
     console.log(costOf);
-    document.querySelector("div#result").removeAttribute("class");
     h2.append("Your pizza has been submitted.");
-    paragraph.append("Your total is $" + costOf +". Made with love and turtle power");
+    paragraph.append("Your total is $" + costOf +". Made with love and turtle power!");
     document.body.append(h2, paragraph);
   }
   
-  function hideResults(event) {
-    event.preventDefault()
-    const h2Hide = document.querySelector("h2");
-    const pHide = document.querySelector("p")
-    h2Hide.remove();
-    pHide.remove();
-  }
   
   window.addEventListener("load", function () {
     document.querySelector("form#orderUp").addEventListener("submit", handlePizzaOrder);
-    hideResults();
   });
